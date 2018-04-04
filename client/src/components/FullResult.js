1dbdf7a822d4ebb8cf69ddb575624ac0375134e3
Agnieszka Domanska <domanska.a.m.87@gmail.com>
2018-04-04 20:39:10 +0100

    Reserve space for Result when it's not displayed. (#7)

--- client/src/components/FullResult.js
+++ client/src/components/FullResult.js
@@ -2,18 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const FullResult = ({currA, currB, names, currAval, result}) => {
-  if (currA && currB && currAval && result) {
-    const nameA = currAval > 1 ? names[currA].name_plural : names[currA].name;
-    const nameB = result > 1 ? names[currB].name_plural : names[currB].name;
-    return (
-      <div>
-        <p className='result-first-line'> {currAval} {nameA} equals </p>
-        <p className='result-second-line'> {result} {nameB} </p>
-      </div>
-    );
-  } else {
-    return null;
+  let nameA = 'Needs to be calculated!';
+  let nameB = 'Needs to be calculated!';
+  let isDataSet = currA && currB && currAval && result;
+  if (isDataSet) {
+    nameA = currAval > 1 ? names[currA].name_plural : names[currA].name;
+    nameB = result > 1 ? names[currB].name_plural : names[currB].name;
   }
+  return (
+    <div>
+      <p className={isDataSet ? 'result-first-line' : 'result-first-line hidden'}> {currAval} {nameA} equals </p>
+      <p className={isDataSet ? 'result-second-line' : 'result-second-line hidden'}> {result} {nameB} </p>
+    </div>
+  );
 };
 
 export default FullResult;
