17f15b6d9774325d083daf5a20f442450e6a70cf
Agnieszka Domanska <domanska.a.m.87@gmail.com>
2018-04-12 11:52:29 +0100

    merge master with branch #3

--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export class Result extends React.Component {
-/*  constructor (props) {
-    super(props);
+const Result = ({currA, currB, currAval, currBval, names}) => {
+  let nameA = 'Needs to be calculated!';
+  let nameB = 'Needs to be calculated!';
+  let isDataSet = currA && currB && currAval && currBval;
+  if (isDataSet) {
+    nameA = currAval > 1 ? names[currA].name_plural : names[currA].name;
+    nameB = currBval > 1 ? names[currB].name_plural : names[currB].name;
   }
-*/
+  return (
+    <div>
+      <p className={isDataSet ? 'result-first-line' : 'result-first-line hidden'}> {currAval} {nameA} equals </p>
+      <p className={isDataSet ? 'result-second-line' : 'result-second-line hidden'}> {currBval} {nameB} </p>
+    </div>
+  );
+};
 
-  render () {
-    return (
-      <div>
-        <p className='result'> = {this.props.result ? this.props.result : ''} </p>
-      </div>
-    );
-  }
-}
+export default Result;
 
 Result.propTypes = {
-  result: PropTypes.number
+  currA: PropTypes.string,
+  currB: PropTypes.string,
+  currAval: PropTypes.number,
+  currBval: PropTypes.number,
+  names: PropTypes.object
 };
